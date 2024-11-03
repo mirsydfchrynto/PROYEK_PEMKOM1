1b5bd05e28130b3b66518cf972929247238efcb7
mirsydfchrynto <mirsyadf1805@gmail.com>
2024-11-03 21:55:31 +0700

    pertemuan_6

---
+++ src/aplikasi/koneksi.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aplikasi;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author HP
+ */;
+
+public class koneksi {
+    public static Connection Go(){
+        try {
+            MysqlDataSource m = new MysqlDataSource();
+            m.setServerName("localhost");
+            m.setDatabaseName("pos"); 
+            m.setUser("root"); 
+            m.setPassword(""); 
+            m.setPortNumber(3306); 
+            m.setServerTimezone("Asia/Jakarta");
+            Connection c = m.getConnection();
+            //System.out.println("Sukses terhubung");
+            return c;
+        } catch (SQLException e) {
+            //System.err.println("GAGAL terhubung\n"+e.getMessage());
+        }
+        
+        return null;
+    }
+}
---
+++ src/aplikasi/userprofile.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aplikasi;
+
+/**
+ *
+ * @author HP
+ */
+public class userprofile {
+    private int id;
+    private String nama;
+    private String username;
+    private String password;
+    private String level;
+    private String alamat;
+    private String telepon;
+    private String email;
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+    
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+    
+    public String getTelepon() {
+        return telepon;
+    }
+
+    public void setTelepon(String telepon) {
+        this.telepon = telepon;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
